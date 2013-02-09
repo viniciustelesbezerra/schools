f730cf22566a30f5c1bbf9d6daeb5a7e66d9b95a
Vinicius Teles <viniciusteles.bez@gmail.com>
2013-02-09 14:43:19 -0200

    Removing uncessary flash message and adding nested attributes for turmas

--- app/models/turma.rb
+++ app/models/turma.rb
@@ -1,5 +1,6 @@
 class Turma < ActiveRecord::Base
 	has_many :disciplinas_turmas, dependent: :destroy
 	has_many :disciplinas, through: :disciplinas_turmas
+  	accepts_nested_attributes_for :disciplinas
   	attr_accessible :name
 end
