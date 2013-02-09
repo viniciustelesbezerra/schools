7ddfdc2ba3f35b890acdc1df57bfed7156bf907d
Vinicius Teles <viniciusteles.bez@gmail.com>
2013-02-09 15:46:04 -0200

    Sending new button to add more disciplinas to turma

--- app/models/disciplina.rb
+++ app/models/disciplina.rb
@@ -2,5 +2,5 @@ class Disciplina < ActiveRecord::Base
 	belongs_to :professor
 	has_many :disciplinas_turmas, dependent: :destroy
 	has_many :turmas, through: :disciplinas_turmas
-  	attr_accessible :name
+  	attr_accessible :name, :professor_id
 end
