4dff6a43c5ec4bed9877ea98ebe2e7633c1b5a82
Vinicius Teles <viniciusteles.bez@gmail.com>
2013-02-10 21:07:06 -0200

    Sending root path to app

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Sistemaescola::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'disciplinas#index'
+  root :to => 'turmas#index'
 
   # See how all your routes lay out with "rake routes"
 
