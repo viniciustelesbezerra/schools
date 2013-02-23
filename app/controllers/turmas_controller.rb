6a22f358d21fd91ea72befbd8f682532e97797ee
Vinicius Teles <viniciusteles.bez@gmail.com>
2013-02-23 13:57:15 -0300

    Drying the code

--- app/controllers/turmas_controller.rb
+++ app/controllers/turmas_controller.rb
@@ -37,16 +37,7 @@ class TurmasController < ApplicationController
       found_ids = []
       @turma_disciplinas.find_each{ |ids_disciplina| found_ids.push(ids_disciplina.id) }
 
-      params[:disciplinas][:id].each do |id_disciplina| 
-        found_ids.delete(id_disciplina.to_i) if found_ids.include?(id_disciplina.to_i)
-        
-        begin
-          @turma_disciplinas.find(id_disciplina)
-        rescue
-          @turma_disciplinas.push(Disciplina.find(id_disciplina))
-        end
-
-      end     
+      params[:disciplinas][:id].each{ |id_disciplina| found_ids.include?(id_disciplina.to_i) ? found_ids.delete(id_disciplina.to_i) : @turma_disciplinas.push(Disciplina.find(id_disciplina)) }
 
       found_ids.each do |ids_to_delete|
         @relation = @turma_disciplinas.find(ids_to_delete)
@@ -66,6 +57,7 @@ class TurmasController < ApplicationController
   end
   
   private
+
   def get_turma(turma_id)
     Turma.find(turma_id)
   end
